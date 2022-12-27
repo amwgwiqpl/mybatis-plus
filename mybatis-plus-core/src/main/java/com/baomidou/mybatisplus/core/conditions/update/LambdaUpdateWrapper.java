f5811fe901071fc6df2f2df71c5285614f9820b7
Shine <234822599@qq.com>
2022-12-27 10:40:58 +0800

    扩展Update的set sql 片段

--- mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/update/LambdaUpdateWrapper.java
+++ mybatis-plus-core/src/main/java/com/baomidou/mybatisplus/core/conditions/update/LambdaUpdateWrapper.java
@@ -15,6 +15,11 @@
  */
 package com.baomidou.mybatisplus.core.conditions.update;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import com.baomidou.mybatisplus.core.conditions.AbstractLambdaWrapper;
 import com.baomidou.mybatisplus.core.conditions.SharedString;
 import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
@@ -22,11 +27,6 @@ import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Lambda 更新封装
@@ -36,7 +36,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @SuppressWarnings("serial")
 public class LambdaUpdateWrapper<T> extends AbstractLambdaWrapper<T, LambdaUpdateWrapper<T>>
-    implements Update<LambdaUpdateWrapper<T>, SFunction<T, ?>> {
+        implements Update<LambdaUpdateWrapper<T>, SFunction<T, ?>> {
 
     /**
      * SQL 更新字段内容，例如：name='1', age=2
@@ -61,8 +61,8 @@ public class LambdaUpdateWrapper<T> extends AbstractLambdaWrapper<T, LambdaUpdat
     }
 
     LambdaUpdateWrapper(T entity, Class<T> entityClass, List<String> sqlSet, AtomicInteger paramNameSeq,
-                        Map<String, Object> paramNameValuePairs, MergeSegments mergeSegments, SharedString paramAlias,
-                        SharedString lastSql, SharedString sqlComment, SharedString sqlFirst) {
+            Map<String, Object> paramNameValuePairs, MergeSegments mergeSegments, SharedString paramAlias,
+            SharedString lastSql, SharedString sqlComment, SharedString sqlFirst) {
         super.setEntity(entity);
         super.setEntityClass(entityClass);
         this.sqlSet = sqlSet;
@@ -91,6 +91,17 @@ public class LambdaUpdateWrapper<T> extends AbstractLambdaWrapper<T, LambdaUpdat
         return typedThis;
     }
 
+    public LambdaUpdateWrapper<T> setSql(boolean condition, SFunction<T, ?> column, String valueSql, Object... values) {
+        if (!condition || StringUtils.isBlank(valueSql)) {
+            return typedThis;
+        }
+        if (StringUtils.MP_SQL_PLACE_HOLDER.matcher(valueSql).matches()) {
+            valueSql = formatSqlMaybeWithParam(valueSql, null, values);
+        }
+        sqlSet.add(columnToString(column) + Constants.EQUALS + valueSql);
+        return typedThis;
+    }
+
     @Override
     public String getSqlSet() {
         if (CollectionUtils.isEmpty(sqlSet)) {
@@ -102,7 +113,8 @@ public class LambdaUpdateWrapper<T> extends AbstractLambdaWrapper<T, LambdaUpdat
     @Override
     protected LambdaUpdateWrapper<T> instance() {
         return new LambdaUpdateWrapper<>(getEntity(), getEntityClass(), null, paramNameSeq, paramNameValuePairs,
-            new MergeSegments(), paramAlias, SharedString.emptyString(), SharedString.emptyString(), SharedString.emptyString());
+                new MergeSegments(), paramAlias, SharedString.emptyString(), SharedString.emptyString(),
+                SharedString.emptyString());
     }
 
     @Override
